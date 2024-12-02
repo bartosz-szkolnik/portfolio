e0fe96f00f30fd95ec2511d3e402533393f428cf
Bartosz Szkolnik <bartosz.szkolnik@protonmail.com>
2024-12-02 01:11:05 +0100

    Add about me section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import { AboutMeSection } from '@components/about-me';
 import { Sidebar } from '@components/sidebar';
 
 export default function Home() {
   return (
-    <div className="bg-background min-h-screen">
-      <div className="container max-w-screen-lg mx-auto px-4 py-8">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+    <div className="min-h-screen bg-background">
+      <div className="container mx-auto max-w-screen-lg px-4 py-8">
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
           <Sidebar />
+          <main className="md:col-span-2">
+            <AboutMeSection />
+          </main>
         </div>
       </div>
     </div>
---
+++ src/components/about-me.tsx
@@ -0,0 +1,14 @@
+import { Card, CardContent, CardHeader, CardTitle } from './common/card';
+
+export function AboutMeSection() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>About me</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p className="text-muted-foreground">I am a software enginner...</p>
+      </CardContent>
+    </Card>
+  );
+}
